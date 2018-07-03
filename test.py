a5bed14c7b64834a7470db7ac059cfcb810bcbf0
Zhiyuan "Erick" Li <ericklek1994@gmail.com>
2018-07-03 10:48:30 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,33 @@
-import logging, sys
+import logging
+import sys
 import pandas as pd
-from pyspark import SparkContext
+from pyspark.sql import SparkSession
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 logging.getLogger("py4j").setLevel(logging.ERROR)
 
-sc = SparkContext()
+spark = SparkSession\
+        .builder\
+        .appName("PythonPi")\
+        .getOrCreate()
 
 dir1 = '/gpfs01/hadoop/user/cng10/Erick/zillow/stores/'
-# dir2 = dir1 + '02/'
+dir2 = dir1 + '02/'
 
-layoutZAsmt = pd.read_excel(dir1+'Layout.xlsx', sheetname=0)
-layoutZTrans = pd.read_excel(dir1+'Layout.xlsx', sheetname=1)
+layoutZAsmt = pd.read_excel(dir1+'Layout.xlsx', sheet_name=0)
+layoutZTrans = pd.read_excel(dir1+'Layout.xlsx', sheet_name=1)
 
 logging.info('Finish loading layout')
 
-col_namesMain = layoutZAsmt[layoutZAsmt['TableName'] == 'utMain', 'FieldName'].T
-col_namesBldg = layoutZAsmt[layoutZAsmt['TableName'] == 'utBuilding', 'FieldName'].T
-col_namesBldgA = layoutZAsmt[layoutZAsmt['TableName'] == 'utBuildingAreas', 'FieldName'].T
+col_namesMain = layoutZAsmt.loc[layoutZAsmt['TableName'] == 'utMain', 'FieldName'].T
+col_namesBldg = layoutZAsmt.loc[layoutZAsmt['TableName'] == 'utBuilding', 'FieldName'].T
+col_namesBldgA = layoutZAsmt.loc[layoutZAsmt['TableName'] == 'utBuildingAreas', 'FieldName'].T
+
+base = pd.read_table(dir2+"ZAsmt/Main.txt", sep='|', header=None, low_memory=False, names=col_namesMain)
+
+print(base.head())
+print(base.info())
+
+base1 = spark.createDataFrame(base)
+
+print(type(base1))
