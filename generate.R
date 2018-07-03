fe5882ecc5cf466256f4ad2820dd5943428b04e9
zehaoc2 <chen.jeremy2@gmail.com>
2018-07-03 14:55:18 -0500

    update

--- generate.R
+++ generate.R
@@ -65,40 +65,40 @@ for (state in abbr) {
 
   #####################################
   # Running the Hedonics
-  
+
   {
     ######################################################################
     # Prepare the tables and fields for extraction Zillow Data
     # Change directory to where you've stored ZTRAX
     # dir <- paste0(DB_path,"DB", state_code) #"DB17"
     dir <- paste0(DB_path, state_code) #"DB17"
-    
+
     if (!file.exists(dir)) {
       print(paste("no folder for DB", toString(state_code), "found for ", state))
       missing_states <- c(missing_states, state)
       next
     }
-    
+
     ## the state code can be find at:
     ## https://en.wikipedia.org/wiki/Federal_Information_Processing_Standard_state_code
     layoutZAsmt <- read_excel(file.path(DB_path, 'Layout.xlsx'), sheet = 1) #("../stores/", 'Layout.xlsx'), sheet = 1)
     layoutZTrans <- read_excel(file.path(DB_path, 'Layout.xlsx'), sheet = 2, col_types = c("text", "text", "numeric", "text", "text"))
-    
+
     ## always NOT do prototyping
     rows2load <- -1 # load everything
     options(scipen = 999) # Do not print scientific notation
     options(stringsAsFactors = FALSE) ## Do not load strings as factors
-    
+
     ##  Create property attribute table
     #    Need 3 tables
     #    1) Main table in assessor database
     #    2) Building table
     #    3) BuildingAreas
-    
+
     col_namesMain <- t(layoutZAsmt[layoutZAsmt$TableName == 'utMain', 'FieldName'])
     col_namesBldg <- t(layoutZAsmt[layoutZAsmt$TableName == 'utBuilding', 'FieldName'])
     col_namesBldgA <- t(layoutZAsmt[layoutZAsmt$TableName == 'utBuildingAreas', 'FieldName'])
-    
+
     ######################################################################
     # Pull address, geographic, lot size, and tax data from main table
     ## get main table
@@ -113,7 +113,7 @@ for (state in abbr) {
                        quote = "",                                # this tells R not to read quotation marks as a special symbol
                        col.names = col_namesMain
     )
-    
+
     base <- as.data.table(base)
     base <- base[ , list(RowID, ImportParcelID, LoadID,
                          FIPS, State, County,
@@ -130,17 +130,17 @@ for (state in abbr) {
     # ImportParcelID is the unique identifier of a parcel. Multiple entries for the same ImportParcelID are due to updated records.
     # The most recent record is identified by the greatest LoadID.
     #   **** This step may not be necessary for the published dataset as we intend to only publish the updated records, but due dilligence demands we check.
-    
+
     if (length(unique(base$ImportParcelID)) != dim(base)[1] ) {
       #Example: Print all entries for parcels with at least two records.
       base[ImportParcelID %in% base[duplicated(ImportParcelID), ImportParcelID], ][order(ImportParcelID)]
-      
+
       setkeyv(base, c("ImportParcelID", "LoadID"))  # Sets the index and also orders by ImportParcelID, then LoadID increasing
       keepRows <- base[ , .I[.N], by = c("ImportParcelID")]   # Creates a table where the 1st column is ImportParcelID and the second column
       # gives the row number of the last row that ImportParcelID appears.
       base <- base[keepRows[[2]], ] # Keeps only those rows identified in previous step
     }
-    
+
     ######################################################################
     #### Load most property attributes
     print(paste("Load most property of", state))
@@ -154,7 +154,7 @@ for (state in abbr) {
                        quote = "",                                # this tells R not to read quotation marks as a special symbol
                        col.names = col_namesBldg
     )
-    
+
     bldg <- as.data.table(bldg)
     bldg <- bldg[, list(RowID, NoOfUnits, BuildingOrImprovementNumber,
                          YearBuilt, EffectiveYearBuilt, YearRemodeled,
@@ -162,8 +162,8 @@ for (state in abbr) {
                          FullBath, ThreeQuarterBath, HalfBath, QuarterBath,
                          HeatingTypeorSystemStndCode,
                          PropertyLandUseStndCode, WaterStndCode)]
-    
-    
+
+
     #  Reduce bldg dataset to Single-Family Residence, Condo's, Co-opts (or similar)
     print(paste("Reducing bldg dataset of", state))
     bldg <- bldg[PropertyLandUseStndCode %in% c('RR101',  # SFR
@@ -180,14 +180,14 @@ for (state in abbr) {
                                                 'RR119',  # Garden Home
                                                 'RR120'), # Landominium
                  ]
-    
+
     # NEW in v300
     attr <- merge(base, bldg, by = "RowID")
     rm(base)
     rm(bldg)
     gc()
     ########
-    
+
     ######################################################################
     #### Load building squarefoot data
     print(paste("Load building sqft data of", state))
@@ -201,10 +201,10 @@ for (state in abbr) {
                        quote = "",                                # this tells R not to read quotation marks as a special symbol
                        col.names = col_namesBldgA
     )
-    
-    
+
+
     sqft <- as.data.table(sqft)
-    
+
     # Counties report different breakdowns of building square footage and/or call similar concepts by different names.
     # The structure of this table is to keep all entries reported by the county as they are given. See 'Bldg Area' table in documentation.
     # The goal of this code is to determine the total square footage of each property.
@@ -219,29 +219,29 @@ for (state in abbr) {
                                              'BAT',  # Building Area Total
                                              'BLF'), # Building Area Finished Living
                  ]
-    
+
     table(sqft$BuildingOrImprovementNumber)  # BuildingOrImprovementNumber > 1  refers to additional buildings on the parcel.
-    
+
     sqft <- sqft[, list(sqfeet = max(BuildingAreaSqFt, na.rm = T)), by = c("RowID", "BuildingOrImprovementNumber")]
-    
+
     ###############################################################################
     #   Merge previous three datasets together to form attribute table
-    
+
     # attr <- merge(base, bldg, by = "RowID")
     attr <- merge(attr, sqft, by = c("RowID", "BuildingOrImprovementNumber"), all.x=TRUE)
-    
+
     ## write an intermediate file ##
     saveRDS(attr, paste0(hedonics_output, "../../temp_attr/", state, ".rds"))
-    
+
     # rm(base)
     # rm(bldg)
     rm(sqft)
-    
+
     # NEW in v300
     rm(attr)
     ###########
     gc() ## collecting garbage
-    
+
     ###############################################################################
     #  Load transaction dataset.
     #     Need two tables
@@ -250,7 +250,7 @@ for (state in abbr) {
     print(paste("Loading transaction dataset of", state))
     col_namesProp <- t(layoutZTrans[layoutZTrans$TableName == 'utPropertyInfo', 'FieldName'])
     col_namesMainTr <-t(layoutZTrans[layoutZTrans$TableName == 'utMain', 'FieldName'])
-    
+
     ###############################################################################
     #   Load PropertyInfo table for later merge
     print(paste("Load propertyinfo table of", state))
@@ -264,33 +264,33 @@ for (state in abbr) {
                             quote = "",                                # this tells R not to read quotation marks as a special symbol
                             col.names = col_namesProp
     )
-    
+
     propTrans <- as.data.table(propTrans)
-    
+
     propTrans <- propTrans[, list(TransId, PropertySequenceNumber, LoadID, ImportParcelID)]
-    
+
     ## we do that
-    
+
     # Keep only one record for each TransID and PropertySequenceNumber.
     # TransID is the unique identifier of a transaction, which could have multiple properties sequenced by PropertySequenceNumber.
     # Multiple entries for the same TransID and PropertySequenceNumber are due to updated records.
     # The most recent record is identified by the greatest LoadID.
     #   **** This step may not be necessary for the published dataset as we intend to only publish most updated record.
-    
+
     setkeyv(propTrans, c("TransId", "PropertySequenceNumber", "LoadID"))
     keepRows <- propTrans[ ,.I[.N], by = c("TransId", "PropertySequenceNumber")]
     propTrans <- propTrans[keepRows[[3]], ]
     propTrans[, LoadID:= NULL]
-    
+
     # Drop transactions of multiple parcels (transIDs associated with PropertySequenceNumber > 1)
-    
+
     dropTrans <- unique(propTrans[PropertySequenceNumber > 1, TransId])
     propTrans <- propTrans[!(TransId %in% dropTrans), ]   # ! is "not"
-    
+
     rm(dropTrans)
     rm(keepRows)
     gc()
-    
+
     #######################################################################################
     #  Load main table in Ztrans database, which provides information on real estate events
     print(paste("Load main Ztrans table of", state))
@@ -304,7 +304,7 @@ for (state in abbr) {
                         quote = "",                                # this tells R not to read quotation marks as a special symbol
                         col.names = col_namesMainTr
     )
-    
+
     trans <- as.data.table(trans)
     trans <- trans[, list(TransId, LoadID,
                            RecordingDate, DocumentDate, SignatureDate, EffectiveDate,
@@ -315,30 +315,30 @@ for (state in abbr) {
                            PartialInterestTransferStndCode, IntraFamilyTransferFlag, TransferTaxExemptFlag,
                            PropertyUseStndCode, AssessmentLandUseStndCode,
                            OccupancyStatusStndCode)]
-    
+
     # Keep only one record for each TransID.
     # TransID is the unique identifier of a transaction.
     # Multiple entries for the same TransID are due to updated records.
     # The most recent record is identified by the greatest LoadID.
     #   **** This step may not be necessary for the published dataset as we intend to only publish most updated record.
-    
+
     setkeyv(trans, c("TransId", "LoadID"))
     keepRows <- trans[, .I[.N], by = "TransId"]
     trans <- trans[keepRows[[2]], ]
     trans[, LoadID:= NULL]
-    
+
     rm(keepRows)
     gc()
     #  Keep only events which are deed transfers (excludes mortgage records, foreclosures, etc. See documentation.)
     trans <- trans[DataClassStndCode %in% c('D', 'H', 'F', 'M'), ] ## D: Deed Transfer, H: Mortage, F: foreclosure, M: mortage
-    
+
     ###############################################################################
     #   Merge previous two datasets together to form transaction table
     transComplete <- merge(propTrans, trans, by = "TransId")
     rm(propTrans)
     rm(trans)
     gc()
-    
+
     ###############################################################################
     #   Merge the trans and azmt table together
     print(paste("getting finalResult of", state))
@@ -347,7 +347,7 @@ for (state in abbr) {
     rm(transComplete)
     rm(attr)
     gc()
-    
+
     ###############################################################################
     #   write the the final merging result
     WRITEFILE = TRUE
@@ -355,7 +355,7 @@ for (state in abbr) {
       print(paste0("Writing to file", paste0(hedonics_output, state, "Hedonics.rds")))
       saveRDS(finalResult, paste0(hedonics_output, state, "Hedonics.rds")) # /home/bdeep/share/projects/Zillow_Housing/stores/Hedonics/states/MIHedonics.rds
     }
-    
+
     rm(finalResult)
     # rm(dropTrans)
     gc()
