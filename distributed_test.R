8ac70077c452445d661f13e3095b31ea0b797553
zehaoc2 <chen.jeremy2@gmail.com>
2018-07-12 15:29:29 -0500

    Update

--- distributed_test.R
+++
@@ -1,2 +0,0 @@
-file <- "/gpfs01/hadoop/user/cng10/Erick/zillow/stores/ca_hedonics.csv"
-# a <- read.csv(file,header=TRUE)
--- test.R
+++ test.R
@@ -1,7 +1,11 @@
 
 #### test file for sparkr. this code only handles a small state of AK.
 
+#### this test temporarily partitions the top 100 rows of the dataset, for the
+####  sake of the memory error..
+
 library(SparkR)
+## from https://stackoverflow.com/questions/38577939/not-able-to-retrieve-data-from-sparkr-created-dataframe
 # Sys.setenv("SPARKR_SUBMIT_ARGS"="--master yarn-client sparkr-shell")
 
 rm(list=ls())
@@ -18,11 +22,12 @@ PkgTest <- function(x) {
 packages <- c("readxl", "data.table", "rgdal", "sp")
 lapply(packages, PkgTest)
 
-sparkR.session(ter = "local[*]", sparkConfig = list(spark.driver.memory = "4g"))
+sparkR.session()
+# namedConfig <- sparkR.conf("spark.executor.memory", "4g")
 # setLogLevel("ERROR")
 
 dir <- "/gpfs01/hadoop/user/cng10/Erick/zillow/stores/"
-dir2 <- paste(dir, "02/", sep = "")
+dir2 <- paste(dir, "46/", sep = "")
 
 #("../stores/", 'Layout.xlsx'), sheet = 1)
 layoutZAsmt <- read_excel(file.path(dir, 'Layout.xlsx'), sheet = 1)
@@ -44,6 +49,16 @@ col_namesMain <- t(layoutZAsmt[layoutZAsmt$TableName == 'utMain', 'FieldName'])
 col_namesBldg <- t(layoutZAsmt[layoutZAsmt$TableName == 'utBuilding', 'FieldName'])
 col_namesBldgA <- t(layoutZAsmt[layoutZAsmt$TableName == 'utBuildingAreas', 'FieldName'])
 
+
+
+
+
+
+
+##################
+####   BASE   ####
+##################
+
 base <- read.table(file.path(dir2, "ZAsmt/Main.txt"),
                        nrows = rows2load,
                        sep = '|',
@@ -55,10 +70,18 @@ base <- read.table(file.path(dir2, "ZAsmt/Main.txt"),
                        col.names = col_namesMain
 )
 
+# base_s <- head(base, n = 100L)
+
 print("finished reading table")
+print(object.size(base))
+print(dim(base))
 # object.size()
 #### using sparkr dataframe object
-base1 <- as.DataFrame(base)
+base1 <- as.DataFrame(base, numPartitions = 100)
+#base1 <- as.DataFrame(as.matrix(base))
+
+#base1 <- read.text(file.path(dir2, "ZAsmt/Main.txt"))
+#base1 <- read.df(paste0(dir2, "ZAsmt/Main.txt"), source = "csv", delimiter = "|", header="true", inferSchema="true")
 
 print("finished converting dataframe")
 
@@ -72,6 +95,7 @@ base1 <- select(base1, list("RowID", "ImportParcelID", "LoadID", "FIPS", "State"
                             "PropertyAddressLatitude", "PropertyAddressLongitude", "PropertyAddressCensusTractAndBlock",
                             "NoOfBuildings", "LotSizeAcres", "LotSizeSquareFeet", "TaxAmount", "TaxYear"))
 
+print("finished selection")
 ##### TODO: convert this part into SparkR style.
     if (length(unique(base$ImportParcelID)) != dim(base)[1] ) {
       #Example: Print all entries for parcels with at least two records.
@@ -82,6 +106,79 @@ base1 <- select(base1, list("RowID", "ImportParcelID", "LoadID", "FIPS", "State"
       # gives the row number of the last row that ImportParcelID appears.
       base <- base[keepRows[[2]], ] # Keeps only those rows identified in previous step
     }
+
+print(base1)
+
+
+
+
+
+
+
+
+##################
+####   BLDG   ####
+##################
+
+
+
+######################################################################
+#### Load most property attributes
+
+bldg <- read.table(file.path(dir2, "ZAsmt/Building.txt"),
+                   nrows = rows2load,                    # this is set just to test it out. Remove when code runs smoothly.
+                   sep = '|',
+                   header = FALSE,
+                   stringsAsFactors = FALSE,
+                   skipNul = TRUE,                            # tells R to treat two ajacent delimiters as dividing a column
+                   comment.char="",                           # tells R not to read any symbol as a comment
+                   quote = "",                                # this tells R not to read quotation marks as a special symbol
+                   col.names = col_namesBldg
+)
+
+print("finished read.table")
+
+# bldg_s <- head(bldg, n = 100L)
+
+bldg <- as.DataFrame(bldg, numPartition = 100)
+
+print("finished as.DataFrame")
+bldg <- select(bldg, c("RowID", "NoOfUnits", "BuildingOrImprovementNumber",
+                     "YearBuilt", "EffectiveYearBuilt", "YearRemodeled",
+                     "NoOfStories", "StoryTypeStndCode", "TotalRooms", "TotalBedrooms",
+                     "FullBath", "ThreeQuarterBath", "HalfBath", "QuarterBath",
+                     "HeatingTypeorSystemStndCode",
+                     "PropertyLandUseStndCode", "WaterStndCode"))
+
+print("finished select")
+#  Reduce bldg dataset to Single-Family Residence, Condo's, Co-opts (or similar)
+
+bldg <- filter(bldg, bldg$PropertyLandUseStndCode %in% c('RR101',  # SFR
+                                                         'RR999',  # Inferred SFR
+                                                         'RR104',  # Townhouse
+                                                         'RR105',  # Cluster Home
+                                                         'RR106',  # Condominium
+                                                         'RR107',  # Cooperative
+                                                         'RR108',  # Row House
+                                                         'RR109',  # Planned Unit Development
+                                                         'RR113',  # Bungalow
+                                                         'RR116',  # Patio Home
+                                                         'RR119',  # Garden Home
+                                                         'RR120')) # Landominium
+
+print("finished reducing bldg dataset")
+
+
+
+
+
+
+###### merging !!!!!
+attr <- merge(base1, bldg, by = "RowID")
+
+print("finished merging")
+print(attr)
+
 
 
 print("hello, world.")
